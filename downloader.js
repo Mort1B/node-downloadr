2ae6495bbf56b77f1b45a1537e8a8680b2738b68
Morten Bergseng <bergseng.m@gmail.com>
2022-10-18 18:11:46 +0200

    Update downloader.js
    Added second way of doing the task

--- downloader.js
+++ downloader.js
@@ -37,25 +37,44 @@ const getAndDecodeChunkData = async (offset) => {
     .catch((e) => console.error("Error getting chunk data ", e));
 };
 
+
+// There are two ways of doing this, either(1) by starting at the size/ length of the vector we are given from calling the
+// getAndDecodeChunkData function and subtracting the length/size of each vector of bytes until we reach offset - size.
+// The other way(2) is by subtracting the size before the loop and adding the length/size of each vector of bytes until we reach the
+// end_offset.
+// By commenting *in* line 50, 67-77 and commenting *out* line 55-67 you can run the code that does it the second way(2).
 const writeToFile = async (tx) => {
   const result = await getTxOffsetData(tx);
   const size = parseInt(result.size);
   const offset = parseInt(result.offset);
-  const offsetStart = offset - size + 1;
+//   const offsetStart = offset - size + 1;
 
   const data = new Uint8Array(size);
 
-  for(let byte = 0; byte < size; byte) {
-    const chunkData = await getAndDecodeChunkData(offsetStart + byte);
-    
+  let chunk = offset;
+
+  while (chunk > offset - size) {
+    const chunkData = await getAndDecodeChunkData(chunk);
+
     if (chunkData) {
-        data.set(chunkData, byte);
-        byte += chunkData.length;
+        data.set(chunkData, offset - chunk);
+        chunk -= chunkData.length;
     } else {
-        throw new Error(`Error at ${byte}/${size}`);
+        throw new Error(`Error - cannot find chunk data, make sure input is correct and try again`)
     }
-    console.log(Math.trunc(100 * byte / size), '%');
   }
+
+//   for(let byte = 0; byte < size; byte) {
+//     const chunkData = await getAndDecodeChunkData(offsetStart + byte);
+//
+//     if (chunkData) {
+//         data.set(chunkData, byte);
+//         byte += chunkData.length;
+//     } else {
+//         throw new Error(`Error at ${byte}/${size}`);
+//     }
+//     console.log(Math.trunc(100 * byte / size), '%');
+//   }
 
   fs.writeFile(`./${argv.output}`, data, (e) => {
     if (e) return console.error("Error writing to file: ", e);
